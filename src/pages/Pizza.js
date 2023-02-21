075a0c37628d1086c99676c53d02c6c0c466b626
ZacharyDonovan88 <zachary.donovan88@outlook.com>
2023-02-21 21:19:55 +1030

    updated style on pizza page

--- src/pages/Pizza.js
+++ src/pages/Pizza.js
@@ -1,10 +1,16 @@
 import React from "react";
+import "../App.css";
 
 const Pizza = () => {
   
     return (
       <>
-        <h2>Pizza</h2>
+        <div className="pagesSection">
+            <h2>Pizza</h2>
+                <div className="menuSection">
+
+                </div>
+        </div>
       </>
     );
   };
