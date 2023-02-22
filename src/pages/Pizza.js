fea239aae944a0f261078e7e9c83ad9835c235d6
ZacharyDonovan88 <zachary.donovan88@outlook.com>
2023-02-22 16:06:11 +1030

    updated pizza section

--- src/pages/Pizza.js
+++ src/pages/Pizza.js
@@ -1,16 +1,32 @@
 import React from "react";
 import "../App.css";
+import "./css/menu.css";
+import { Link } from 'react-router-dom';
 
 const Pizza = () => {
   
     return (
-      <>
+      <> 
         <div className="pagesSection">
-            <h2>Pizza</h2>
-                <div className="menuSection">
-
-                </div>
+            <h2>Traditional Pizza</h2> 
         </div>
+        
+            <div className="pagesSection">
+                <div className="menuSection">
+                        <div>
+
+                            <a>
+                                <div>
+                                    <a href="" target="_blank"><img src={require('../components/assets/images/pizza/pep.jpg')} alt="Circle WebSite"/></a>
+                                        <div className="productName">
+                                            <a><Link to="/">Pepperoni</Link></a>
+                                        </div>
+                                </div>
+                            </a>
+
+                        </div>
+                    </div>
+            </div>
       </>
     );
   };
