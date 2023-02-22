137169058e06c1b5c7c75254f80aa112c17e4b7a
ZacharyDonovan88 <zachary.donovan88@outlook.com>
2023-02-22 17:44:26 +1030

    pizza page updated

--- src/pages/Pizza.js
+++ src/pages/Pizza.js
@@ -15,15 +15,90 @@ const Pizza = () => {
                 <div className="menuSection">
                         <div>
 
-                            <a>
-                                <div>
-                                    <a href="" target="_blank"><img src={require('../components/assets/images/pizza/pep.jpg')} alt="Circle WebSite"/></a>
-                                        <div className="productName">
-                                            <a><Link to="/">Pepperoni</Link></a>
-                                        </div>
-                                </div>
-                            </a>
+                        
+                            <div className="boxMargin">
+                                <a href="" target="_blank"><img src={require('../components/assets/images/pizza/pep.jpg')} alt="Circle WebSite"/></a>
+                                    <div className="productName">
+                                        <a><Link to="/">Pepperoni</Link></a>
+                                    </div>
+                            </div>
+                        
+                            <div className="boxMargin">
+                                <a href="" target="_blank"><img src={require('../components/assets/images/pizza/mar.jpg')} alt="Circle WebSite"/></a>
+                                    <div className="productName">
+                                        <a><Link to="/">Margherita</Link></a>
+                                    </div>
+                            </div>
+                        
+                            <div className="boxMargin">
+                                <a href="" target="_blank"><img src={require('../components/assets/images/pizza/meat.jpg')} alt="Circle WebSite"/></a>
+                                    <div className="productName">
+                                        <a><Link to="/">All Meat</Link></a>
+                                    </div>
+                            </div>
 
+                            <div className="boxMargin">
+                                <a href="" target="_blank"><img src={require('../components/assets/images/pizza/sup.jpg')} alt="Circle WebSite"/></a>
+                                    <div className="productName">
+                                        <a><Link to="/">Supreme</Link></a>
+                                    </div>
+                            </div>
+
+                            <div className="boxMargin">
+                                <a href="" target="_blank"><img src={require('../components/assets/images/pizza/veg.jpg')} alt="Circle WebSite"/></a>
+                                    <div className="productName">
+                                        <a><Link to="/">Vegetarian</Link></a>
+                                    </div>
+                            </div>
+
+                            <div className="boxMargin">
+                                <a href="" target="_blank"><img src={require('../components/assets/images/pizza/chick.jpg')} alt="Circle WebSite"/></a>
+                                    <div className="productName">
+                                        <a><Link to="/">Chicken</Link></a>
+                                    </div>
+                            </div>
+
+                        </div>
+                    </div>
+            </div>
+
+            <div className="pagesSection">
+            <h2>Gourmet Pizza</h2> 
+        </div>
+        
+            <div className="pagesSection">
+                <div className="menuSection">
+                        <div>
+
+                        
+                            <div className="boxMargin">
+                                <a href="" target="_blank"><img src={require('../components/assets/images/pizza/cream.jpg')} alt="Circle WebSite"/></a>
+                                    <div className="productName">
+                                        <a><Link to="/">Cream Chicken</Link></a>
+                                    </div>
+                            </div>
+
+                            <div className="boxMargin">
+                                <a href="" target="_blank"><img src={require('../components/assets/images/pizza/gveg.jpg')} alt="Circle WebSite"/></a>
+                                    <div className="productName">
+                                        <a><Link to="/">Gourmet Veg</Link></a>
+                                    </div>
+                            </div>
+
+                            <div className="boxMargin">
+                                <a href="" target="_blank"><img src={require('../components/assets/images/pizza/sea.jpg')} alt="Circle WebSite"/></a>
+                                    <div className="productName">
+                                        <a><Link to="/">Seafood</Link></a>
+                                    </div>
+                            </div>
+
+                            <div className="boxMargin">
+                                <a href="" target="_blank"><img src={require('../components/assets/images/pizza/prawn.jpg')} alt="Circle WebSite"/></a>
+                                    <div className="productName">
+                                        <a><Link to="/">Prawn Special</Link></a>
+                                    </div>
+                            </div>
+                        
                         </div>
                     </div>
             </div>
